b6b872a18cba4d05747c5f7dfc33a1241c0eeacc
Blessedimmaculate <nowenakazibwe7@gmail.com>
2024-07-04 08:16:25 +0300

    first commit

--- objectsAndarrays.js
+++ objectsAndarrays.js
@@ -1,12 +1,13 @@
-//custom object
+//custom object cari brackects/braces 
 let car ={
     make: "Toyota",
     model: "Corona",
     year: 2022,
     color: "silver",
+    //properties and value
     start: function(){//method{} if not inside an object, is a function
         console.log("The car has started");
-    }
+    }//if u hv a function within an element is called a METHOD
 };
 console.log(car);
 //car is an object so, no parenthesis
